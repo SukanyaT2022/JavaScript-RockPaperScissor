0c0a0c62307154898bfc631cbe40c121c2bae2c3
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-08-09 21:01:06 +0700

    add

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-const { Img } = require("@chakra-ui/react");
+
 
 var round = 1
 var playerScore = 0
 var robotScore = 0
 function nextRound(){
+ 
+  if(round < 6){
     round = round + 1; 
-    if(round == 5){
-        if(playerScore>robotScore){
-           document.getElementById('resultID').innerText = ('Player wins the match.')
-        }else if(  robotScore>playerScore){
-         document.getElementById('resultID').innerText = ('Robot wins the match.')
-        }else{
-     document.getElementById('resultID').innerText = ('The match ia a draw')
-        }
-    }else{
         document.getElementById('round').innerText = 'Round '+ round
         document.getElementById("rockID").style.visibility = 'visible'
         document.getElementById("paperID").style.visibility = 'visible'
         document.getElementById("scissorID").style.visibility = 'visible'
     document.getElementById('robotID').src ='https://images.unsplash.com/photo-1527430253228-e93688616381?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9ib3R8ZW58MHx8MHx8fDA%3D'
-    }
+   document.getElementById('nextButtonId').disabled = true; // click next round is disable
+}
    
 }
-
 
 function makeRockImgShow(){
     document.getElementById("rockID").style.visibility = 'visible'
@@ -30,6 +23,8 @@ function makeRockImgShow(){
     document.getElementById("scissorID").style.visibility = 'hidden'
     var robot = robotRandom()
     whoWin(1,robot)// checking who the winner player or robot- 1 is player choice and robot choice
+    document.getElementById('nextButtonId').disabled = false; 
+    // after choose a choice then the button able to go to next round
 }
 function makePaperImgShow(){
     document.getElementById("rockID").style.visibility = 'hidden'
@@ -37,6 +32,8 @@ function makePaperImgShow(){
     document.getElementById("scissorID").style.visibility = 'hidden'
     var robot = robotRandom()
     whoWin(2,robot)// 2 player choose choice number 2
+    document.getElementById('nextButtonId').disabled = false; 
+    // after choose a choice then the button able to go to next round
 }
 function makeScissorImgShow(){
     document.getElementById("rockID").style.visibility = 'hidden'
@@ -44,20 +41,22 @@ function makeScissorImgShow(){
     document.getElementById("scissorID").style.visibility = 'visible'
     var robot = robotRandom()
     whoWin(3,robot)
+    document.getElementById('nextButtonId').disabled = false; 
+    // after choose a choice then the button able to go to next round
 }
 
 function robotRandom() {
   var randomNum =  Math.floor(Math.random() * (3 - 1 + 1) + 1);
   if(randomNum==1){
     //rock
-    document.getElementById('robotID').src = "https://images.unsplash.com/photo-1458501534264-7d326fa0ca04?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cm9ja3xlbnwwfHwwfHx8MA%3D%3D"
+    document.getElementById('robotID').src = "/image/rock.webp"
   }else if(randomNum==2){
     //paper
-document.getElementById('robotID').src = "https://images.unsplash.com/photo-1532153955177-f59af40d6472?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHBhcGVyfGVufDB8fDB8fHww" 
+document.getElementById('robotID').src = "/image/paper.webp" 
   }
   else{
     //scissor
-    document.getElementById('robotID').src = "https://images.unsplash.com/photo-1534978184044-62700a717864?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Npc3NvcnxlbnwwfHwwfHx8MA%3D%3D" 
+    document.getElementById('robotID').src = "/image/scissor.webp" 
   }
   return randomNum
 }
@@ -95,6 +94,16 @@ function whoWin(player, robot){
     }
     document.getElementById('playerScoreID').innerText = "Player's score = " + playerScore
     document.getElementById('robotScoreID').innerText = "Robot's score = " + robotScore
+
+    if(round == 6){
+        if(playerScore>robotScore){
+           document.getElementById('resultID').innerText = ('Player wins the match.')
+        }else if(  robotScore>playerScore){
+         document.getElementById('resultID').innerText = ('Robot wins the match.')
+        }else{
+     document.getElementById('resultID').innerText = ('The match is a draw')
+        }
+    }
 }
 
 
